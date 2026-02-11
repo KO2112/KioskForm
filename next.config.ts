b864ee1a3bf4b80aea1be20bace5d11090bab117
Kemal Orhan <kemal182182@gmail.com>
2026-02-11 13:52:25 +0000

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  redirects: async () => {
-    return [
-      {
-        source: "/:path*",
-        destination: "https://cabinunits.co.uk/:path*",
-        permanent: true, // 301 redirect
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
